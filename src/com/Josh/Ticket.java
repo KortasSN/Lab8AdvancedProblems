5a0dc3531ac635728c9e8eb6ce3669bc4926f1a7
Destro <kortas@gmail.com>
2016-03-24 15:16:51 -0500

    Initial commit

---
+++ src/com/Josh/Ticket.java
@@ -0,0 +1,64 @@
+package com.Josh;
+import java.util.Date;
+import java.util.*;
+
+/**
+ * Created by Destro on 3/22/2016.
+ */
+public class Ticket {
+
+    private int priority;
+    private String reporter; //Stores person or department who reported issue
+    private String description;
+    private Date dateReported;
+
+
+    //We can autogenerate get and set methods if and when we need
+
+    //A constructor would be useful
+
+    public Ticket(String desc, int p, String rep, Date date) {
+        this.description = desc;
+        this.priority = p;
+        this.reporter = rep;
+        this.dateReported = date;
+        }
+
+    //Called automatically if a Ticket object is an argument to System.out.println
+    public String toString(){
+        return(this.description + " Priority: " + this.priority + " Reported by: "
+                + this.reporter + " Reported on: " + this.dateReported);
+    }
+    protected static void deleteTicket(LinkedList<Ticket> ticketQueue) {
+        printAllTickets(ticketQueue);   //display list for user
+
+        if (ticketQueue.size() == 0) {    //no tickets!
+            System.out.println("No tickets to delete!\n");
+            return;
+        }
+
+        Scanner deleteScanner = new Scanner(System.in);
+        System.out.println("Enter ID of ticket to delete");
+        int deleteID = deleteScanner.nextInt();
+
+        //Loop over all tickets. Delete the one with this ticket ID
+        boolean found = false;
+        for (Ticket ticket : ticketQueue) {
+            if (ticket.getTicketID() == deleteID) {
+                found = true;
+                ticketQueue.remove(ticket);
+                System.out.println(String.format("Ticket %d deleted", deleteID));
+                break; //don't need loop any more.
+            }
+        }
+        if (found == false) {
+            System.out.println("Ticket ID not found, no ticket deleted");
+            //TODO – re-write this method to ask for ID again if not found
+        }
+        printAllTickets(ticketQueue);  //print updated list
+
+    }
+
+
+
+}
