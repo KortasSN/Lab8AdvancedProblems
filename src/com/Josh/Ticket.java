0316c4d9d2414cfdf6d55feee6e01a8caf38e97a
kortasthekiller <kortas@gmail.com>
2016-03-29 17:06:21 -0500

    running now

--- src/com/Josh/Ticket.java
+++ src/com/Josh/Ticket.java
@@ -7,6 +7,38 @@ import java.util.*;
  */
 public class Ticket {
 
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateReported() {
+        return dateReported;
+    }
+
+    public void setDateReported(Date dateReported) {
+        this.dateReported = dateReported;
+    }
+
     private int priority;
     private String reporter; //Stores person or department who reported issue
     private String description;
@@ -29,8 +61,11 @@ public class Ticket {
         return(this.description + " Priority: " + this.priority + " Reported by: "
                 + this.reporter + " Reported on: " + this.dateReported);
     }
+
+
     protected static void deleteTicket(LinkedList<Ticket> ticketQueue) {
-        printAllTickets(ticketQueue);   //display list for user
+        TicketManager outputTickets = new TicketManager();
+        outputTickets.printAllTickets(ticketQueue);   //display list for user
 
         if (ticketQueue.size() == 0) {    //no tickets!
             System.out.println("No tickets to delete!\n");
@@ -41,21 +76,21 @@ public class Ticket {
         System.out.println("Enter ID of ticket to delete");
         int deleteID = deleteScanner.nextInt();
 
-        //Loop over all tickets. Delete the one with this ticket ID
-        boolean found = false;
-        for (Ticket ticket : ticketQueue) {
-            if (ticket.getTicketID() == deleteID) {
-                found = true;
-                ticketQueue.remove(ticket);
-                System.out.println(String.format("Ticket %d deleted", deleteID));
-                break; //don't need loop any more.
-            }
-        }
-        if (found == false) {
-            System.out.println("Ticket ID not found, no ticket deleted");
-            //TODO – re-write this method to ask for ID again if not found
-        }
-        printAllTickets(ticketQueue);  //print updated list
+//        Loop over all tickets. Delete the one with this ticket ID
+//        boolean found = false;
+//        for (Ticket ticket : ticketQueue) {
+//            if (ticket.getTicketID() == deleteID) {
+//                found = true;
+//                ticketQueue.remove(ticket);
+//                System.out.println(String.format("Ticket %d deleted", deleteID));
+//                break; //don't need loop any more.
+//            }
+//        }
+//        if (found == false) {
+//            System.out.println("Ticket ID not found, no ticket deleted");
+//            //TODO – re-write this method to ask for ID again if not found
+//        }
+        outputTickets.printAllTickets(ticketQueue);  //print updated list
 
     }
 
